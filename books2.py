04de8c610f1895de4850b73d4e32073a98f7ff4d
Alyoninthecity <53126736+Alyoninthecity@users.noreply.github.com>
2025-09-25 11:43:45 +0200

    Section 6 - Endpoint responses and refine data validation

--- books2.py
+++ books2.py
@@ -1,29 +1,42 @@
 from typing import Optional
-from fastapi import FastAPI,Body, HTTPException
+from fastapi import FastAPI,Body, HTTPException, Path,Query
 from pydantic import BaseModel, Field
+from datetime import datetime
+from starlette import status
+'''
+GET status_code=status.HTTP_200_OK
+POST status_code=status.HTTP_201_CREATED
+PUT status_code=status.HTTP_200_OK oppure status_code=status.HTTP_204_NO_CONTENT
+'''
 
 app=FastAPI()
 
+
+def get_current_year():
+    return datetime.now().year
 class Book:
     id: int
     title: str
     author: str
     description: str
     rating: int
+    publish_date: int
     
-    def __init__(self,id,title,author,description,rating):
+    def __init__(self,id,title,author,description,rating,publish_date=get_current_year()):
         self.id=id
         self.title=title
         self.author=author
         self.description=description
         self.rating=rating
+        self.publish_date=publish_date
 
 class BookRequest(BaseModel):
     id: Optional[int]=Field(description="Id non necessario in create", default=None) #Mettere sempre None se voglio aggiungere Field
     title: str = Field(min_length=3,max_length=100)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1)
-    rating: int = Field(gt=0,lt=6) #Da 1 a 5
+    rating: int = Field(gt=0,lt=6,description="Rating da 1 a 5") #Da 1 a 5
+    publish_date : int = Field(ge=0,le=get_current_year(), description="Anno di pubblicazione")
     
     #VALORI DI DEFAULT PER IL JSON TRAMITE /DOCS
     model_config= {
@@ -32,7 +45,8 @@ class BookRequest(BaseModel):
                 "title":"Nuovo libro",
                 "author":"Autore",
                 "description":"Descrizione libro",
-                "rating":5
+                "rating":5,
+                "publish_date":get_current_year()
             }
         }
     }
@@ -47,11 +61,11 @@ BOOKS=[
     Book(6,"Computer Science5","codingwithjomama","Nice booko5",5),
 ]
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
-@app.post("/create-book/")
+@app.post("/create-book/", status_code=status.HTTP_201_CREATED)
 async def create_book(book_request: BookRequest):
     new_book=Book(**book_request.model_dump()) #Altrimenti sto passando un BookRequest dentro ai miei libri se non creo un oggetto Book
     print(type(new_book))
@@ -64,15 +78,15 @@ def find_last_book_id(book:Book):
         book.id = 1
     return book
 
-@app.get("/books/{book_id}/")
-async def reab_book_id(book_id: int):
+@app.get("/books/{book_id}/", status_code=status.HTTP_200_OK)
+async def read_book_id(book_id: int= Path(gt=0)):
     for book in BOOKS:
         if book_id == book.id:
             return book
     raise HTTPException(status_code=404,detail="Libro non trovato")
 
-@app.get("/books/sort_by_rating/")
-async def reab_book_by_rating(book_rating: int):
+@app.get("/books/sort_by_rating/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int=Query(gt=0,lt=6)):
     book_ok=[]
     for book in BOOKS:
         if book_rating == book.rating:
@@ -82,3 +96,29 @@ async def reab_book_by_rating(book_rating: int):
     else:
         return book_ok
     
+
+@app.put("/books/update_book",status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book:BookRequest):
+    books_updated=False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id==book.id:
+            BOOKS[i]=book
+            books_updated=True
+    if not books_updated:
+        raise HTTPException(status_code=404,detail="Libro non trovato")
+
+@app.delete("/books/{book_id}",status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id:int=Path(gt=0)):
+    for i in BOOKS:
+        if i.id==book_id:
+            BOOKS.remove(i)
+            return i
+    raise HTTPException(status_code=404,detail="Libro non trovato")
+
+@app.get("/books/sort_by_publish_date", status_code=status.HTTP_200_OK)
+async def read_books_by_publish_date(publish_date:int=Query(gt=1990)):
+    book_to_return=[]
+    for book in BOOKS:
+        if book.publish_date==publish_date:
+            book_to_return.append(book)
+    return book_to_return
